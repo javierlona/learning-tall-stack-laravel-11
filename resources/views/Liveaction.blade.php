b1b63749f9cb23fa492a0b8cb07b50b1dcf6e179
Javier Lona <javierlona1@gmail.com>
2025-02-21 21:02:20 -0600

    Fix Liveaction blade

--- resources/views/Liveaction.blade.php
+++ resources/views/Liveaction.blade.php
@@ -1,5 +1,14 @@
 {{-- The content inside the <x-layouts.app> tags will be injected into the {{ $slot }} directive in the resources/views/components/layouts/app.blade.php layout file. --}}
 <x-layouts.app>
+    <x-slot:title>
+        Live Action Blade
+    </x-slot>
+    <x-slot:the-apple>
+        <h2 class="font-mono text-orange-600">The Apple Components</h1>
+        <div>
+            <h2>Will appear below $slot </h2>
+        </div>
+    </x-slot>
     <div class="container">
         <h1>Livewire Components</h1>
         <div>
@@ -8,7 +17,7 @@
         </div>
         <div>
             <h2>Form Component</h2>
-            @livewire('form')
+            <livewire:form />
         </div>
     </div>
 </x-layouts.app>
--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -10,5 +10,6 @@
     </head>
     <body>
         {{ $slot }}
+        {{ $theApple }}
     </body>
 </html>
